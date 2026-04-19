592aab19de16e4e819faf9b7d464e3b5a38039cc
HITESH-235 <hiteshsinha2007@gmail.com>
2026-04-19 06:05:22 +0000

    fix build script

--- render-build.sh
+++ render-build.sh
@@ -2,11 +2,11 @@
 # exit on error
 set -o errexit
 
+# Install Python dependencies
+pip install -r requirements.txt
+
 # Build the frontend
 cd frontend
 npm install
 npm run build
 cd ..
-
-# Install Python dependencies
-pip install -r requirements.txt
